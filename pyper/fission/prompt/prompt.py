ee3b4783654aee348c366de37d1b51dd25fd4422
Daniel Lee <dannylee1020@gmail.com>
2025-01-19 21:18:35 -0800

    update fission prompt

--- pyper/fission/prompt/prompt.py
+++ pyper/fission/prompt/prompt.py
@@ -1,14 +1,13 @@
 breadth_prompt = """
 You are an expert prompt engineer responsible for generating instruction tasks for building dataset.
-Your job is to create exactly {batch} new tasks inspired by the base tasks given.
+Your job is to create exactly {batch} creative version of new tasks inspired by the base tasks given.
 
 Generation Guideline:
 1. New task should be in the same domain as the information in the base tasks.
-2. Vary sentence structure, vocabularly and tone for maximum diversity
-3. The new task has new concepts and is distinct from the original task
-4. The new task has equal level of difficulty as the original task
-5. The new task has the similar length to the original task.
-6. The new task only has factual information and is logically understandable by human
+2. The new task has a new concept that is distinct from the base tasks.
+3. The new task has equal level of difficulty as the original task
+4. The new task has the similar length to the original task.
+5. The new task only has factual information and is logically understandable by human
 
 Here are the base tasks:
 {base_tasks}
@@ -16,7 +15,7 @@ Here are the base tasks:
 
 depth_prompt = """
 You are an expert prompt engineer responsible for generating instruction tasks for building dataset.
-Your job is to create exactly {batch} more complex tasks inspired by the base tasks given.
+Your job is to create exactly {batch} complex version of new tasks inspired by the base tasks given.
 
 
 Generation Guideline:
